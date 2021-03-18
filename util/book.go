e115a762668cb9541b9fc7cbe2304a921af67d82
Joe Martinson <joe.martinson@modalai.com>
2021-03-18 15:31:58 -0700

    fix ordering in handlebook

--- util/book.go
+++ util/book.go
@@ -10,18 +10,16 @@ import (
 
 func HandleBook(command string, s *discordgo.Session, m *discordgo.MessageCreate) {
 	spaced_words := strings.Split(command, " ")
-	var book string
 	if len(spaced_words) != 2 {
 		s.ChannelMessageSend(m.ChannelID, "please specify one book")
 		return
 	}
-	book = spaced_words[1]
-	var r io.Reader
-	r, err := os.Open("books/" + spaced_words[1])
+	book := spaced_words[1]
+
 	dir, _ := os.ReadDir("books")
 	exists := false
 	for i := 0; i < len(dir); i++ {
-		if strings.Compare(dir[i].Name(), book) == 0 {
+		if dir[i].Name() == book {
 			exists = true
 		}
 	}
@@ -29,9 +27,13 @@ func HandleBook(command string, s *discordgo.Session, m *discordgo.MessageCreate
 		s.ChannelMessageSend(m.ChannelID, "book does not exist")
 		return
 	}
+
+	var r io.Reader
+	r, err := os.Open("books/" + spaced_words[1])
 	if err != nil {
 		s.ChannelMessageSend(m.ChannelID, "book does not exist")
 		return
 	}
+
 	s.ChannelFileSend(m.ChannelID, spaced_words[1], r)
 }
