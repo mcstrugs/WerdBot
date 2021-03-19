122e2ede9b48624b306b1a168142620a6d767457
joemartinson <martinsonjoe2@gmail.com>
2021-03-18 18:28:17 -0700

    implement best match using fuzzy matching

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mcstrugs/WerdBot
 
 go 1.16
 
-require github.com/bwmarrin/discordgo v0.23.2
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/sahilm/fuzzy v0.1.0 // indirect
+)
--- util/book.go
+++ util/book.go
@@ -1,11 +1,13 @@
 package util
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/sahilm/fuzzy"
 )
 
 func HandleBook(command string, s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -17,23 +19,24 @@ func HandleBook(command string, s *discordgo.Session, m *discordgo.MessageCreate
 	book := spaced_words[1]
 
 	dir, _ := os.ReadDir("books")
-	exists := false
+	var book_names []string
 	for i := 0; i < len(dir); i++ {
-		if dir[i].Name() == book {
-			exists = true
-		}
+		book_names = append(book_names, dir[i].Name())
 	}
-	if !exists {
-		s.ChannelMessageSend(m.ChannelID, "book does not exist")
+	fmt.Println(book_names)
+	matches := fuzzy.Find(book, book_names)
+	if len(matches) == 0 {
+		s.ChannelMessageSend(m.ChannelID, "no books matching: "+book)
 		return
 	}
+	best_match := matches[0].Str
 
 	var r io.Reader
-	r, err := os.Open("books/" + spaced_words[1])
+	r, err := os.Open("books/" + best_match)
 	if err != nil {
 		s.ChannelMessageSend(m.ChannelID, "book does not exist")
 		return
 	}
 
-	s.ChannelFileSend(m.ChannelID, book, r)
+	s.ChannelFileSend(m.ChannelID, best_match, r)
 }
